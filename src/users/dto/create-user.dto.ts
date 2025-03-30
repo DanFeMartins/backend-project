6d9110ded04f3d3bf95895c156c7364fcc1ade5a
daniel <danielfemartins@gmail.com>
2025-03-30 20:44:24 -0300

    feat: implementa CRUD and ODT para users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,25 @@
+import { isEmail, IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  name?: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  senha: string;
+}
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  name?: string;
+
+  @IsOptional()
+  @MinLength(6)
+  senha?: string;
+}
